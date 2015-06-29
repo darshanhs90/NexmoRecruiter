491be3321130fa8847cc0382028b9ed2bcf48a12
Haridarshan HS <hsdars@gmail.com>
2015-06-29 09:11:04 -0500

    nexmo api

---
+++ nexmo.js
@@ -0,0 +1,53 @@
+var https=require('https');
+
+//number verify
+/*https.get('https://api.nexmo.com/verify/json?api_key=638c2b46&api_secret=60539549&number=14697672278&brand=NexmoVerifyTest',
+        function(response) {
+            var body = '';
+            response.on('data', function(d) {
+                body += d;
+            });
+            response.on('end', function() {
+
+                // Data reception is done, do whatever with it!
+                var parsed = JSON.parse(body);
+                console.log(parsed);
+                
+            });
+
+        });*/
+
+//message
+/*https.get('https://rest.nexmo.com/sms/json?api_key=638c2b46&api_secret=60539549&from=12092664035&to=14697672278&text=Welcome+to+Nexmo',
+        function(response) {
+            var body = '';
+            response.on('data', function(d) {
+                body += d;
+            });
+            response.on('end', function() {
+
+                // Data reception is done, do whatever with it!
+                var parsed = JSON.parse(body);
+                console.log(parsed);
+                
+            });
+
+        });*/
+
+//call ivr
+/*https.get('https://api.nexmo.com/tts/xml?api_key=638c2b46&api_secret=60539549&to=14697672278&text=Welcome+to+Nexmo',
+        function(response) {
+            var body = '';
+            response.on('data', function(d) {
+                body += d;
+            });
+            response.on('end', function() {
+
+                // Data reception is done, do whatever with it!
+                var parsed = JSON.parse(body);
+                console.log(parsed);
+                
+            });
+
+        });*/
+
