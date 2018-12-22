592009dc82c0c4c3c334cc54f50b144fe3142458
Haridarshan HS <hsdars@gmail.com>
2018-12-22 15:19:16 +0530

    Update app.js

--- app.js
+++ app.js
@@ -11,21 +11,21 @@ var http=require('http');
 var cors = require('cors');
 var Twitter = require('twitter');
 var client = new Twitter({
-    consumer_key: 'LmNp3JwAQZnuBr4SQFaM7UZG3',
-    consumer_secret: 'Xps6ziqIhZ0exAPoIAeyqj7myu7L78ZLHQDni67dzD9koJQTAD',
-    access_token_key: '151128859-F4Wk8KebqH4ZDwp8tMWY8PkoTQzfiEJrN1t2Knfc',
-    access_token_secret: 'czQre16YZKoC4Csi18gGufu8PxF733aL5VnzbhurlGvHw'
+    consumer_key: 'consumer_key',
+    consumer_secret: 'consumer_secret',
+    access_token_key: 'access_token_key',
+    access_token_secret: 'access_token_secret'
 });
 var watson = require('watson-developer-cloud');
 var AlchemyAPI = require('alchemy-api');
-var alchemy = new AlchemyAPI('7b6bf4773c39c9e271f6bd999fea5df5179a6dad');
-var sendgrid = require('sendgrid')('hsdars', 'Password90-');
-var accountSid = 'AC07275e4294f1b0d42623c3ec9559911e';
-var authToken = '650d049a9bd99323fb899ce4b9e84fcc';
+var alchemy = new AlchemyAPI('id');
+var sendgrid = require('sendgrid')('id', 'pwd');
+var accountSid = 'accountSid';
+var authToken = 'authToken';
 var clientTwilio = require('twilio')(accountSid, authToken);
 var speech_to_text = watson.speech_to_text({
-    username: '1a4e2a43-2a65-4e28-b9bc-2947c6a48e47',
-    password: 'WNMkUbFzLf6c',
+    username: 'username',
+    password: 'password',
     version: 'v1'
 });
 var qnArray=[];
@@ -97,7 +97,7 @@ rspns.end(emailId);
 //set phonenumber
 app.get('/getPhone', function(reqst, rspns) {
     if(phoneNumber=='')
-        phoneNumber='4697672278';
+        phoneNumber='num';
 rspns.end(phoneNumber);
 
 });
@@ -113,7 +113,7 @@ rspns.end(companyName);
 app.get('/verifycode', function(reqst, rspns) {
     var number=reqst.query.number;
     console.log(number);
-https.get('https://api.nexmo.com/verify/json?api_key=638c2b46&api_secret=60539549&number=1'+number+'&brand=NexRuiter Verify',
+https.get('https://api.nexmo.com/verify/json?api_key=api_key&api_secret=api_secret&number=1'+number+'&brand=NexRuiter Verify',
         function(response) {
             var body = '';
             response.on('data', function(d) {
@@ -133,7 +133,7 @@ https.get('https://api.nexmo.com/verify/json?api_key=638c2b46&api_secret=6053954
 app.get('/verifycheck', function(reqst, rspns) {
 var code=reqst.query.code;
 var reqstid=reqst.query.requestId;
-https.get('https://api.nexmo.com/verify/check/json?api_key=638c2b46&api_secret=60539549&request_id='+reqstid+'&code='+code,
+https.get('https://api.nexmo.com/verify/check/json?api_key=api_key&api_secret=api_secret&request_id='+reqstid+'&code='+code,
         function(response) {
             var body = '';
             response.on('data', function(d) {
@@ -164,7 +164,7 @@ app.get('/message', function(reqst, rspns) {
         text='Congrats+You+have+been+shortlisted+for+a+job';
     else
         text='Congrats+You+have+been+offered+for+a+job';
-https.get('https://rest.nexmo.com/sms/json?api_key=638c2b46&api_secret=60539549&from=12092664035&to=1'+number+'&text='+text,
+https.get('https://rest.nexmo.com/sms/json?api_key=api_key&api_secret=api_secret&from=num&to=1'+number+'&text='+text,
         function(response) {
             var body = '';
             response.on('data', function(d) {
@@ -193,7 +193,7 @@ app.get('/call', function(reqst, rspns) {
         text='Congrats+You+have+been+offered+for+a+job';
     else 
         text=questions[reqst.query.qnnumber];
-    https.get('https://api.nexmo.com/tts/xml?api_key=638c2b46&api_secret=60539549&to=1'+number+'&text='+text,
+    https.get('https://api.nexmo.com/tts/xml?api_key=api_key&api_secret=api_secret&to=1'+number+'&text='+text,
         function(response) {
             var body = '';
             response.on('data', function(d) {
@@ -216,7 +216,7 @@ app.get('/message1', function(reqst, rspns) {
     var number=reqst.query.number;
     var text=reqst.query.text;
     var val='A+Recruiter+From+'+text+'+has+joined+Nexruiter';
-   https.get('https://rest.nexmo.com/sms/json?api_key=638c2b46&api_secret=60539549&from=12092664035&to=1'+number+'&text='+val,
+   https.get('https://rest.nexmo.com/sms/json?api_key=apiKey&api_secret=api_secret&from=num&to=1'+number+'&text='+val,
         function(response) {
             var body = '';
             response.on('data', function(d) {
@@ -239,19 +239,6 @@ app.get('/sendMail', function(reqst, rspns) {
     var toEmailAddress = email;
     var subject=reqst.query.subject;
     var textval=reqst.query.textval;
-    //get from "name","to email address","company name"
-    /*sendgrid.send({
-        to: 'hsdars@gmail.com',
-        from: email,
-        subject: subject,
-        text: textval
-    }, function(err, json) {
-        if (err) {
-            return console.error(err);
-        }
-        console.log(json);
-        rspns.end(json);
-    });*/
     rspns.end('Mail send successfully to '+email);
 
 });
@@ -340,7 +327,7 @@ app.get('/companyInfo', function(reqst, respns) {
     //company website
     var companyName=reqst.query.companyName;
     console.log(companyName);
-    https.get('https://api.fullcontact.com/v2/company/lookup.json?domain='+companyName+'.com&apiKey=f6e2b2695278badc',
+    https.get('https://api.fullcontact.com/v2/company/lookup.json?domain='+companyName+'.com&apiKey=apiKey',
         function(response) {
             var body = '';
             response.on('data', function(d) {
@@ -365,7 +352,7 @@ app.get('/personInfo', function(reqst, respns) {
     //person email id  
     var email=reqst.query.email;
     console.log(email);
-    https.get('https://api.fullcontact.com/v2/person.json?email='+email+'&apiKey=f6e2b2695278badc',
+    https.get('https://api.fullcontact.com/v2/person.json?email='+email+'&apiKey=apiKey',
         function(response) {
             var body = '';
             response.on('data', function(d) {
@@ -403,8 +390,8 @@ http.get('http://nexruiter.webuda.com/retrieve.php',
                  
 
    var personality_insights = watson.personality_insights({
-                                                "username": "51397aaa-2786-4342-9cb7-40f1225de1a7",
-                                                "password": "ufQQJaVROpjw",
+                                                "username": "username",
+                                                "password": "password",
                                                 version: 'v2'
                                             });
 
